73dade7ded163a87cce03865f74adcd442280ab1
Pranav-Varma-03 <102585172+Pranav-Varma-03@users.noreply.github.com>
2022-06-06 10:22:20 +0530

    Add files via upload

---
+++ Assignment 8/code/code.py
@@ -0,0 +1,13 @@
+# Part 1 of solution:
+x=7.7
+if(abs(x-8)<(1.67*(1.5/8))):
+    print("We accept Ho")
+else :
+    print("We reject Ho")
+
+#Part 2 of Solution
+y=7.7
+if(abs(y-8)<(2.62*(1.5/8))):
+    print("We accept Ho")
+else :
+    print("We reject Ho")
